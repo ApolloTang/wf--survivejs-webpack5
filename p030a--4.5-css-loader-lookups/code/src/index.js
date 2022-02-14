cd7412741179e468d63e898409a90fe7ac5cf430
Apollo Tang <tang.apollo@gmail.com>
2022-02-14 03:14:46 -0500

    add css loader lookups

---
+++ p030a--4.5-css-loader-lookups/code/src/index.js
@@ -0,0 +1,9 @@
+import component from './component'
+import main from './main.css'
+
+const elements = component()
+console.log(elements)
+
+document.body.appendChild(elements.green)
+document.body.appendChild(elements.blue)
+document.body.appendChild(elements.red)
