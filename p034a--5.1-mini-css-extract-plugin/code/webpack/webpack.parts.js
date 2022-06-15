5d17050af4a5dc1f76796d50bc3dc65caa463650
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-06-15 10:33:01 -0400

    [p034a] clone p030a --> p034a

---
+++ p034a--5.1-mini-css-extract-plugin/code/webpack/webpack.parts.js
@@ -0,0 +1,58 @@
+const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin')
+const { WebpackPluginServe } = require("webpack-plugin-serve")
+
+const devServer = () => {
+  const out = {
+    watch: true,
+    plugins: [
+     new WebpackPluginServe(
+       {
+         port: process.env.PORT || 8080,
+         static: './dist',
+         liveReload: true,
+         waitForBuild: true,
+         host:'127.0.0.1' // you have set host if you use Safary
+       }
+     )
+   ]
+  }
+  return out
+}
+
+const loadCSS = () => {
+  const out = {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                url: true
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+  return out
+}
+
+const page = ({title}) => {
+  const out = {
+    plugins: [
+      new MiniHtmlWebpackPlugin( {context: { title }} )
+    ]
+  }
+  return out
+}
+
+module.exports = {
+  devServer,
+  loadCSS,
+  page
+}
+
