a87ef28cbe188b83c449148cbe6410c433ca0087
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-07-16 06:03:04 -0400

    automatic configuring "importLoaders" options

--- p038a--6.1-setting-up-tailwind/code/webpack/webpack-part--extract-css.js
+++ p038a--6.1-setting-up-tailwind/code/webpack/webpack-part--extract-css.js
@@ -3,13 +3,16 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const extractCSS = (
   { options_MiniCssExtractPlugin = {}, loaders = [] } = {}
 ) => {
+  // see https://github.com/webpack-contrib/css-loader#importloaders
+  const importLoaders = loaders.length
+
   const module = {
     rules: [
       {
         test: /\.css$/,
         use: [
           { loader: MiniCssExtractPlugin.loader, options: options_MiniCssExtractPlugin },
-          { loader: 'css-loader', options: { importLoaders: 1 } },
+          { loader: 'css-loader', options: { importLoaders } },
         ].concat(loaders),
         sideEffects: true, // <--- if sideEffect is false css will be tree shaked
       },
--- p038a--6.1-setting-up-tailwind/code/webpack/webpack-part--load-css.js
+++ p038a--6.1-setting-up-tailwind/code/webpack/webpack-part--load-css.js
@@ -1,13 +1,16 @@
 const loadCSS = (
   { loaders = [] } = {}
 ) => {
+  // see https://github.com/webpack-contrib/css-loader#importloaders
+  const importLoaders = loaders.length
+
   const module = {
     rules: [
       {
         test: /\.css$/,
         use: [
           'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
+          { loader: 'css-loader', options: { importLoaders } },
         ].concat(loaders),
       }
     ]
