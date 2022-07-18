0f217c60064da39816b47e1d86e604a38823914d
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-07-18 01:44:55 -0400

    6.2 enabling-purgecss (wip)

---
+++ p041a--6.2-enabling-purgecss--(wip)/code/webpack/webpack-part--eleminate-unused-css.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const glob = require('glob');
+const PurgeCSSPlugin = require('purgecss-webpack-plugin');
+
+const ALL_FILES = glob.sync(path.join(__dirname, 'src/*.js'));
+
+eliminateUnusedCSS = () => ({
+  plugins: [
+    new PurgeCSSPlugin({
+      paths: ALL_FILES, // Consider extracting as a parameter
+      extractors: [
+        {
+          extractor: (content) =>
+            content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+          extensions: ['html'],
+        },
+      ],
+    }),
+  ],
+});
+
+module.exports = eliminateUnusedCSS;
---
+++ p041a--6.2-enabling-purgecss--(wip)/code/webpack/webpack.config.js
@@ -0,0 +1,57 @@
+const { mode } = require('webpack-nano/argv')
+const { merge } = require('webpack-merge')
+
+const part_page = require('./webpack-part--page.js')
+const part_devServer = require('./webpack-part--dev-server.js')
+const part_loadCss = require('./webpack-part--load-css.js')
+const part_extractCss = require('./webpack-part--extract-css.js')
+const part_eliminateUnusedCSS = require('./webpack-part--eleminate-unused-css.js')
+
+const cssloader_postcss = require('./webpack-part--cssloader--postcss.js')
+
+const DEBUG_PROD = false
+
+const commonConfig = merge([
+  {
+    entry: [ './src/main.js' ]
+  },
+  part_page({title:'demo'}),
+])
+
+
+const productionConfig = merge([
+  part_extractCss({
+    loaders: [cssloader_postcss()]
+  }),
+  part_eliminateUnusedCSS()
+])
+
+
+const developmentConfig = merge([
+  {
+    entry: [ 'webpack-plugin-serve/client' ]
+  },
+  part_devServer(),
+  part_loadCss({
+    loaders: [cssloader_postcss()]
+  })
+])
+
+
+const getConfig = (mode) => {
+  switch (mode) {
+    case 'production':
+      return merge(commonConfig, productionConfig, {mode: !DEBUG_PROD ? mode : 'none'})
+    case 'development':
+      return merge(commonConfig, developmentConfig, {mode})
+    default:
+      throw new Error(`Trying to use an unknow mode, ${mode}`)
+  }
+
+}
+
+
+const config = getConfig(mode)
+// console.dir(config, {depth: 8})
+
+module.exports = config
