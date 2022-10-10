0d3d58f613ae24c9de7f142ba67fb8cb81d50882
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-10-10 19:37:04 -0400

    [p057a--9.1b-output-assetModuleFilename] done

---
+++ p057a--9.1b-output-assetModuleFilename/code/src/main.js
@@ -0,0 +1,9 @@
+import component from './component'
+import main from './main.css'
+
+const elements = component()
+console.log(elements)
+
+document.body.appendChild(elements.red)
+document.body.appendChild(elements.green)
+document.body.appendChild(elements.cat)
---
+++ p057a--9.1b-output-assetModuleFilename/code/webpack/webpack-part--load-images.js
@@ -0,0 +1,27 @@
+const loadImages = (
+  { limit } = { limit: 1 * 1024 }
+) => {
+  const module = {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: limit // if images size is less than limit, injected into the bundle as a Base64-encoded string.
+          }
+        }
+      }
+    ]
+  }
+
+  return {
+    output: {
+      assetModuleFilename: 'images/[hash][ext][query]',
+    },
+    module
+  }
+}
+
+
+module.exports = loadImages
