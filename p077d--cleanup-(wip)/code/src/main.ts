3a9a3555c711b7b8688ba0599d04fce9e0f58915
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-10-16 02:28:51 -0400

    [p077d--cleanup-(wip)] move asset loading into setup check

--- p077d--cleanup-(wip)/code/src/main.ts
+++ p077d--cleanup-(wip)/code/src/main.ts
@@ -1,12 +1,6 @@
-import component from './component'
 import './main.css'
 
-import ('./setup-check/')
-
-
-
-const elements = component()
-console.log(elements)
+import './setup-check/'
 
 // [NOTE]
 //   To see fonts in action make sure you unzip monrope.zip in ./src/fonts/
@@ -14,11 +8,4 @@ console.log(elements)
 // import ('./fonts/')
 // document.body.appendChild(elements.fontManropeRegular)
 // document.body.appendChild(elements.fontManropeLight)
-
-document.body.appendChild(elements.red)
-document.body.appendChild(elements.green)
-document.body.appendChild(elements.cat)
-document.body.appendChild(elements.imgCopyLeft)
-document.body.appendChild(elements.imgNy1)
-
 
--- p077d--cleanup-(wip)/code/src/setup-check/index.js
+++ p077d--cleanup-(wip)/code/src/setup-check/index.js
@@ -1,6 +1,6 @@
 import  './setup-check--purge-css/'
+import  './setup-check--loading-images/index.js'
 import  './setup-check--js/index.js'
 
-export {}
 
 
