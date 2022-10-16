7f85eaca8167b2f788b94d9cf368f3be2fc3d780
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2022-10-15 23:17:00 -0400

    typo

--- p077d--cleanup-(wip)/code/src/setup-check/setup-check-js/es7-pollyfill-array-include.setup-check.js
+++ p077d--cleanup-(wip)/code/src/setup-check/setup-check-js/es7-pollyfill-array-include.setup-check.js
@@ -1,7 +1,7 @@
 var IE11= !!window.msCrypto // https://stackoverflow.com/questions/21825157/internet-explorer-11-detection
 
 if(!IE11) {
-  console.warn('pollyfill setup check should be tested on IE11')
+  console.warn('pollyfill setup check should be done on IE11')
 }
 
 try {
